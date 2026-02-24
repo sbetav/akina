68e28f90d08d672e080b8917b1e4b771fbb466f2
Santiago <santibeta82@gmail.com>
2026-02-23 20:58:14 -0500

    Setup eslint and prettier configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import { FC } from 'react'
+import { FC } from "react";
 
 const Page: FC = () => {
-  return <div className='min-h-dvh flex items-center justify-center w-full'>Hello World!</div>
-}
+  return (
+    <div className="flex min-h-dvh w-full items-center justify-center">
+      Hello World!
+    </div>
+  );
+};
 
-export default Page
+export default Page;
