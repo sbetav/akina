9e3850ffca4b40321ae6be0fd6e8039174de1672
Santiago Betancur <santibeta82@gmail.com>
2026-04-05 19:13:20 -0500

    Hotfix

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/40 aria-invalid:border-destructive/50 group/button inline-flex shrink-0 cursor-pointer items-center justify-center border bg-clip-padding text-xs font-medium whitespace-nowrap uppercase transition-all outline-none select-none focus-visible:ring-3 disabled:pointer-events-none disabled:opacity-50 aria-invalid:ring-3 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+  "focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/40 aria-invalid:border-destructive/50 group/button inline-flex shrink-0 cursor-pointer items-center justify-center border bg-clip-padding text-xs font-medium whitespace-nowrap uppercase transition-all outline-none select-none focus-visible:ring-3 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:ring-3 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
   {
     variants: {
       variant: {
