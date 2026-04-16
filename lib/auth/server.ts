afb2709f895d1830f1ed77e892319a8684a3c037
Santiago Betancur <santibeta82@gmail.com>
2026-04-16 17:01:14 -0500

    Hotfix

--- lib/auth/server.ts
+++ lib/auth/server.ts
@@ -21,7 +21,7 @@ export const auth = betterAuth({
     schema: authSchema,
   }),
   session: {
-    expiresIn: 24 * 60 * 60 * 1000, // 24 hours
+    expiresIn: 60 * 60 * 24, // 24 hours
   },
   user: {
     fields: {
